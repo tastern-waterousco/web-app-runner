7ba8768e219f4b2fa6aec0401540a27b84895dd0
darryl west <darryl.west@RainCitySoftware.com>
2014-07-25 12:59:44 -0700

    cleaned up daemon example and refactored to run without the daemon flag

--- examples/daemon-server.js
+++ examples/daemon-server.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
+// example of how to create a daemon server without setting the daemon flag
 var dash = require( 'lodash' ),
+    childKey = '--run-child-service',
     args = dash.toArray( process.argv ),
     runner,
     command,
@@ -8,20 +10,28 @@ var dash = require( 'lodash' ),
         port:3002,
         logfile: __dirname + '/daemon-server.log',
         env:'staging',
-        daemon:true
+        daemon:false
     };
 
+// remove the 'node' param
 args.shift();
 
 console.log( args );
 
-if (args[ 1 ] === 'runDaemon') {
+if (args[ 1 ] === childKey ) {
+    // this was called by the background service, so run as a child
     console.log( 'run the web app, pid: ', process.pid );
     runner = require( '../lib/WebAppRunner' ).createInstance( opts );
+    runner.start();
 } else {
-    command = args[ 0 ];
+    // pull the original script name
+    command = args.shift();
+
+    // push our child key to the front of the args list, while preserving any other args
+    args.unshift( childKey );
+
     runner = require('background-service-runner' ).createDaemonRunner();
-    child = runner.start( command, [ 'runDaemon' ] );
+    child = runner.start( command, args );
 
     console.log('child pid: ', child.pid );
 }
