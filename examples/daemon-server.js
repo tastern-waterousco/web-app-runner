0a47fd866805f6e5b2f96d5f2524dd18e8297d75
darryl west <darryl.west@RainCitySoftware.com>
2014-07-25 10:07:12 -0700

    implemented daemon server script

--- examples/daemon-server.js
+++ examples/daemon-server.js
@@ -1,11 +1,28 @@
 #!/usr/bin/env node
 
-var log = require('simple-node-logger').createSimpleLogger( __dirname + '/daemon-server.log' ),
+var dash = require( 'lodash' ),
+    args = dash.toArray( process.argv ),
+    runner,
+    command,
     opts = {
         port:3002,
+        logfile: __dirname + '/daemon-server.log',
+        env:'staging',
         daemon:true
-    },
-    runner = require( '../lib/WebAppRunner' ).createInstance( opts );
+    };
 
-runner.start();
+args.shift();
+
+console.log( args );
+
+if (args[ 1 ] === 'runDaemon') {
+    console.log( 'run the web app, pid: ', process.pid );
+    runner = require( '../lib/WebAppRunner' ).createInstance( opts );
+} else {
+    command = args[ 0 ];
+    runner = require('background-service-runner' ).createDaemonRunner();
+    child = runner.start( command, [ 'runDaemon' ] );
+
+    console.log('child pid: ', child.pid );
+}
 
